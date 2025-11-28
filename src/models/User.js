e64afe15d15629483626d5720105e356bb484c9d
Mariana Perez <marianacruz23@aragon.unam.mx>
2025-11-27 21:57:32 -0600

    conflicto

--- src/models/User.js
+++ src/models/User.js
@@ -8,16 +8,11 @@ const UserSchema = new Schema({
 }, { timestamps: true });
 
 // Middleware para encriptar contraseña antes de guardar
-UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Método para comparar contraseñas
