73075e63df004f09ad988198d6eefb509c098983
Mariana Perez <marianacruz23@aragon.unam.mx>
2025-11-27 20:50:14 -0600

    conflicto

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -7,11 +7,10 @@ const UserSchema = new Schema({
   role: { type: String, enum: ['admin', 'user'], default: 'user' }
 }, { timestamps: true });
 
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre("save", async function() {
+     if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 UserSchema.methods.comparePassword = function (plain) {
