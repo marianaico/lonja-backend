a508bce6e32ff6cebba370d1171394253442e424
Mariana Perez <marianacruz23@aragon.unam.mx>
2025-11-27 20:15:53 -0600

    conflicto

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const authRoutes = require("./routes/auth");
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 app.use(express.json());
